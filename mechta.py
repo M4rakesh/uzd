f482c3264033e7715fac77781000ebe2e41e3f8c
M4rakesh <maksims.rogozins@gmail.com>
2024-10-08 11:34:08 +0300

    Update mechta.py

--- mechta.py
+++ mechta.py
@@ -34,10 +34,18 @@ class Krit:
         self.p_kriterijs = p_k
         self.o_kriterijs = o_k
         self.t_kriterijs = t_k
+
     def krit_info(self):
-        print("Pirmais kriterijs:"+self.p_kriterijs)
-        print("otrais kriterijs:"+ self.o_kriterijs)
-        print("trešāis kriterijs:"+self.t_kriterijs)
+        return [self.p_kriterijs,self.o_kriterijs,self.t_kriterijs]
+
+    def krit_info_print(self):
+        print("Pirmais kriterijs:"+str(self.p_kriterijs))
+        print("Otrais kriterijs:"+ str(self.o_kriterijs))
+        print("Trešāis kriterijs:"+str(self.t_kriterijs))
+
+kri1=Krit("Saturs","Grammatika","Vardu krajums")
+kri2=Krit("Saturs","Grammatika","Vardu krajums")
+kri3=Krit("Saturs","Grammatika","Vardu krajums")
 
 ver1=Vertesana("Marija","Aleksejs","priogrammešāna")
 ver2=Vertesana("Aleksandrs","Mihail","Angļu valoda")
@@ -45,10 +53,18 @@ ver3=Vertesana("Emma","Ksenija","Latviešu valoda")
 print(ver1.darbaId)
 ver1.vertesanas_info()
 ver1.vertesana_info_print()
+kri1.krit_info()
+kri1.krit_info_print()
 print(ver2.darbaId)
 ver2.vertesanas_info()
 ver2.vertesana_info_print()
+kri2.krit_info()
+kri2.krit_info_print()
 print(ver3.darbaId)
 ver3.vertesanas_info()
 ver3.vertesana_info_print()
+kri3.krit_info()
+kri3.krit_info_print()
+
+
 #https://github.com/M4rakesh/uzd/blob/main/mechta.py
